0b2ddbfe066da5cbc374f221f0a90e815cf30848
glriadomenica-debug <glriadomenica@gmail.com>
2026-09-06 20:41:09 +0900

    updaate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import { useRoutes } from "react-router-dom";
 import LoginRoutes from "../modules/Auth/route";
 import DashboardRoutes from "../modules/Dashboard/route";
+import AdminRoutes from "../modules/Admin/route";
 
 export default function AppRoutes() {
-  return useRoutes([...LoginRoutes,
-    ...DashboardRoutes,
-  ]);
+  return useRoutes([...LoginRoutes, ...DashboardRoutes, ...AdminRoutes]);
 }
